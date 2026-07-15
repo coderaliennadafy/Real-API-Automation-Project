074b71e0c6f3969ba2c1060b4b4a95ca931d9ec5
ali ennadafy <aennadafy@gmail.com>
2026-07-15 19:22:41 +0100

    push posts endpoint

--- tests/posts/posts.test.js
+++ tests/posts/posts.test.js
@@ -19,6 +19,22 @@ describe("Posts API Tests", () => {
 
         expect(response.body[0]).to.have.property("body");
 
+        });
+
+    it("should get a post by id", async () => {
+
+        const response = await request.get("/posts/1");
+
+        expect(response.status).to.equal(200);
+
+        expect(response.body).to.be.an("object");
+
+        expect(response.body.id).to.equal(1);
+
+        expect(response.body).to.have.property("title");
+
+        expect(response.body).to.have.property("body");
+
     });
 
 });
