a7bbf3d81a37f7ee63cb5eaad0afa7e2dcdee59a
ali ennadafy <aennadafy@gmail.com>
2026-07-17 21:32:52 +0100

    push 15 tests passing

--- utils/request.js
+++ utils/request.js
@@ -1,27 +1,6 @@
-const request = require('supertest');
-const config = require('../config/config');
+const supertest = require("supertest");
+const config = require("../config/config");
 
-const api = request(config.baseURL);
+const request = supertest(config.baseURL);
 
-async function get(endpoint) {
-    return await api.get(endpoint);
-}
-
-async function post(endpoint, data) {
-    return await api.post(endpoint).send(data);
-}
-
-async function put(endpoint, data) {
-    return await api.put(endpoint).send(data);
-}
-
-async function del(endpoint) {
-    return await api.delete(endpoint);
-}
-
-module.exports = {
-    get,
-    post,
-    put,
-    del
-};
+module.exports = request;
